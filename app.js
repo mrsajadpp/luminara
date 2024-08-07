680aa2277cac6a67ddbfc2737a1ef1834de05c56
mrsajadpp <mrsajadpp@gmail.com>
2024-08-07 17:51:44 +0530

    live

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 app.use('/', logRouter);
 app.use('/', indexRouter);
 
