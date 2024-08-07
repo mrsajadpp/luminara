86c80a080207a8b2ee7ea6146f6c1b7d1af5e85b
mrsajadpp <mrsajadpp@gmail.com>
2024-08-07 19:17:44 +0530

    student id added

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Student = require('../models/Student');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const QRCode = require('qrcode');
+
+async function generateUniqueId() {
+    let uniqueId = await 'LR' + Math.floor(10000 + Math.random() * 90000);
+    let student = await Student.findOne({ studentId: uniqueId }).lean();
+    student ? uniqueId = await 'LR' + Math.floor(10000 + Math.random() * 90000) : null;
+    return uniqueId;
+}
+
 
 // Student Registration Route
 router.get('/register', (req, res) => {
@@ -9,11 +20,39 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res) => {
     const { studentName, registerNumber, standard, division } = req.body;
-    const studentId = await Student.countDocuments() + 1;
+    let student = await Student.findOne({ registerNumber: registerNumber }).lean();
+    if (student) {
+        return res.render('student-register', { title: "Register Student", error: { message: 'Student already registered.' } });
+    }
+    const studentId = await generateUniqueId();
+    console.log(studentId);
+
     try {
+        // Create a document
+        const doc = new PDFDocument();
         const student = new Student({ studentName, registerNumber, standard, division, studentId });
         await student.save();
-        res.redirect('/students/register');
+
+        // Generate QR code
+        const qrData = JSON.stringify({ studentId, studentName, registerNumber, standard, division });
+        const qrCodeUrl = await QRCode.toDataURL(qrData);
+
+        doc.pipe(fs.createWriteStream(studentId + '.pdf'));
+
+        doc.fontSize(16).text('Luminara', { align: 'center' });
+        doc.fontSize(12).text(`Student ID: ${studentId}`);
+        doc.text(`Name: ${studentName}`);
+        doc.text(`Register Number: ${registerNumber}`);
+        doc.text(`Standard: ${standard}`);
+        doc.text(`Division: ${division}`);
+        doc.text(' ');
+        doc.image(qrCodeUrl, { fit: [100, 100], align: 'center' });
+
+        doc.pipe(res);
+
+        doc.end();
+
+        // res.redirect('/students/register');
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
